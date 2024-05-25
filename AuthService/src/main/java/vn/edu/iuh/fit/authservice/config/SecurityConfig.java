16e526721accf22aa1c274b9ecccf443f32b5f01
Tran Quang Huy <tranquanghuyit09@gmail.com>
2024-05-25 19:17:46 +0700

    update Auth Service

--- AuthService/src/main/java/vn/edu/iuh/fit/authservice/config/SecurityConfig.java
+++ AuthService/src/main/java/vn/edu/iuh/fit/authservice/config/SecurityConfig.java
@@ -13,24 +13,62 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig  {
-
+public class SecurityConfig {
 
     @Autowired
     private JwtRequestFilter jwtRequestFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf->csrf.ignoringRequestMatchers("/api/v1/auth/register", "/api/v1/auth/**"));
-        http.authorizeHttpRequests(auth->auth
-                .requestMatchers("/api/v1/auth/register", "api/v1/auth/login", "/api/v1/auth/validate-token").permitAll()//nhung links nay khong can authenticate
+        http.csrf(csrf -> csrf
+                .ignoringRequestMatchers("/api/v1/auth/register", "/api/v1/auth/**")
+        );
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/v1/auth/register", "/api/v1/auth/login", "/api/v1/auth/validate-token").permitAll() // những link này không cần authenticate
                 .requestMatchers("/api/v1/roles", "/api/v1/permissions", "/api/v1/roles/add-permissions").hasRole("ADMIN")
                 .requestMatchers("/h2-console/**").permitAll()
                 .anyRequest().authenticated()
         );
-        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class).csrf().disable();
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
-
-
 }
+
+
+
+//package vn.edu.iuh.fit.authservice.config;
+//
+//import vn.edu.iuh.fit.authservice.filter.JwtRequestFilter;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//
+//@Configuration
+//@EnableWebSecurity
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
+//public class SecurityConfig  {
+//
+//
+//    @Autowired
+//    private JwtRequestFilter jwtRequestFilter;
+//
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http.csrf(csrf->csrf.ignoringRequestMatchers("/api/v1/auth/register", "/api/v1/auth/**"));
+//        http.authorizeHttpRequests(auth->auth
+//                .requestMatchers("/api/v1/auth/register", "api/v1/auth/login", "/api/v1/auth/validate-token").permitAll()//nhung links nay khong can authenticate
+//                .requestMatchers("/api/v1/roles", "/api/v1/permissions", "/api/v1/roles/add-permissions").hasRole("ADMIN")
+//                .requestMatchers("/h2-console/**").permitAll()
+//                .anyRequest().authenticated()
+//        );
+//        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class).csrf().disable();
+//        return http.build();
+//    }
+//
+//
+//}
